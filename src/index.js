a7de0f35f45da3b01313d2bd0c66576b677507cf
unknown <manishabharti236@gmail.com>
2022-06-08 16:56:38 +0530

    errorfour

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect( {
+mongoose.connect("mongodb+srv://Manisha:9831671085@manisha.t8nm5.mongodb.net/manisha1234-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
