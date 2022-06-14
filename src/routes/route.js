902ab7e72be79d2a9fad5394068c50a3c4238a37
unknown <manishabharti236@gmail.com>
2022-06-15 01:21:30 +0530

    jwt_auth1

--- src/routes/route.js
+++ src/routes/route.js
@@ -14,5 +14,6 @@ router.post("/login", userController.loginUser)
 router.get("/users/:userId", userController.getUserData)
 
 router.put("/users/:userId", userController.updateUser)
+router.put("/deleteUsers/:userId", userController.deleteUser)
 
 module.exports = router;
