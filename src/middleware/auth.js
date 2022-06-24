c43936b2f2cd4032e9673021b065ac71d4da31bb
Gaikawade <gmhs6199@gmail.com>
2022-06-24 11:38:47 +0530

    authorId from header

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -15,8 +15,11 @@ const authorization = async function (req, res, next){
     try{
         let token = req.headers[`x-api-key`];
         let decodedToken = jwt.verify(token, "project-blog");
+        let headersData = req.headers;
+        let authorIdFromHeader = headersData[`authorId`];
+        if(!authorIdFromHeader) authorIdFromHeader = headersData[`authorid`];
         let userLoggedIn = decodedToken.userId;
-        let userToBeModified = req.body.authorId || req.params.authorId || req.query.authorId;
+        let userToBeModified = req.body.authorId || req.params.authorId || req.query.authorId || authorIdFromHeader;
         if(userLoggedIn !== userToBeModified) return res.status(403).send({status: false, msg: "You are not authorised to do this"});
         next();
     }
